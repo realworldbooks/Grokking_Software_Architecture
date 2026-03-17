5dc3badbd568c54babfe509b2022e2016fe23fed
realworldbooks <codeliftsleep@realworldbooks.academy>
2026-03-16 22:09:19 -0400

    Add Constants class for server temperature alerts

---
+++ Chapter05/5.4 Server Monitor/After/Core/Domain/Constants.cs
@@ -0,0 +1,8 @@
+namespace ServerMonitor.After.Core.Domain
+{
+    public static class Constants
+    {
+        // Global threshold for server temperature alerts
+        public const int HIGH_TEMP_THRESHOLD = 95;
+    }
+}
